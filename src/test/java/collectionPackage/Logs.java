e63e6ce8f54bc4db7e80ab81d95b00b3e75ad7d0
nandeeshcl <nandeeshclakhi@gmail.com>
2020-03-28 21:55:30 +0530

    annotationsaa

--- src/test/java/collectionPackage/Logs.java
+++ src/test/java/collectionPackage/Logs.java
@@ -5,15 +5,14 @@ import org.apache.log4j.PropertyConfigurator;
 
 public class Logs {
 
-	public static Logger log=Logger.getLogger(Logs.class);
-	
+	public static Logger log = Logger.getLogger(Logs.class);
+
 	public static void main(String[] args) {
-		
+
 		PropertyConfigurator.configure(".\\src\\test\\resources\\configPropery\\log4j.properties");
-		
+
 		log.info("Logs started creeating");
 		log.error("Something went wrong");
-		
 
 	}
 
